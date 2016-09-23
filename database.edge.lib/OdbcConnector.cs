fd8ed4dc97d8b391c87f6184d6e31d15a26034f7
smitchel <smitchel@SMITCHEL-PC>
2016-09-23 01:39:04 -0400

    Add Invoke Implementation

--- database.edge.lib/OdbcConnector.cs
+++ database.edge.lib/OdbcConnector.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using database.edge.lib.providers;
+using database.edge.lib.utility;
+using System.Data;
+using database.edge.lib.conversion;
 
 namespace database.edge.lib
 {
     public class OdbcConnector
     {
-        public  Task<Object> Invoke(dynamic input)
+
+    //    var odbc = {
+    //    dsn:'Test1',
+    //    command:'SELECT * FROM Users WHERE Active={1}',
+    //    params:[{value:'1'}]
+    //};
+
+
+    public async Task<Object> Invoke(dynamic input)
         {
-            return null;
+            object[] sqlparams = { };
+            string dsn = "";
+            string command = "";
+
+            if (OdbcHelper.IsPropertyExist(input, "dsn")){dsn = (string)input.dsn;}
+            if (OdbcHelper.IsPropertyExist(input, "command")){command = (string)input.command;}
+
+            try
+            {
+                sqlparams = (object[])input.@params;
+            }catch (Exception) { }
+
+            IDataBaseProvider db =   OdbcProviderFactory.CreateOdbcProvider("DSN", dsn);
+
+            if (db != null)
+            {
+                //TODO parse parameters and insert into tokensin command
+                string sql = command;
+                DataTable dt  = db.ExecuteSelectQuery(sql);
+
+                //Todo implement formats
+
+                return DataTableConverter.ConvertDataTableToJSON(dt);
+
+            }else
+            {
+                throw new Exception("Provider Not Implemented");
+            }
+
         }
 
 
---
+++ database.edge.lib/utility/OdbcHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database.edge.lib.utility
+{
+    public class OdbcHelper
+    {
+        public static bool IsPropertyExist(dynamic settings, string name)
+        {
+            return settings.GetType().GetProperty(name) != null;
+        }
+    }
+}
